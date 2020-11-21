8d847f26d800cd3586316b157f90de2007b2241e
gtitov <g.titov@marine-rc.ru>
2020-11-22 00:23:52 +0300

    Prototype

--- js/main.js
+++ js/main.js
@@ -8,31 +8,61 @@ var map = new mapboxgl.Map({
     zoom: 11  // starting zoom
 });
 map.addControl(new mapboxgl.NavigationControl());  // zoom and rotation controls
-// modals
-document.addEventListener('DOMContentLoaded', function () {
+    
+
+
+map.on('load', function () {
+    // modals
     var elem = document.querySelector('#modal1');
-    var instance = M.Modal.init(elem, {endingTop: "100%"});
-    map.on('click', function (e) {
-        console.log(e)
-        instance.open()
+    var modal = M.Modal.init(elem, { endingTop: '100%' });
+
+    // points
+    map.addSource('places-points', {
+        type: 'vector',
+        url: 'mapbox://ghermant.ckhry3x4y0tnv23o4m82j7f3t-7jg6k'
+    });
+
+    map.addLayer({
+        'id': 'points-data',
+        'source': 'places-points',
+        'source-layer': 'test',
+        'type': 'symbol',
+        'layout': {
+            'icon-image': 'park-11',
+            'icon-size': 3,
+            'icon-allow-overlap': true,
+        },
+        'paint': {
+            'icon-color': '#FFFF00',
+        }
+        // 'filter': ['==', '$type', 'Point']
+    });
+
+
+    map.on('click', 'points-data', function (e) {
+        var clicked = e.features[0].properties
+
+        document.getElementById("title").innerHTML = clicked.title
+        document.getElementById("address").innerHTML = clicked.address
+        document.getElementById("image").innerHTML = clicked.image
+        document.getElementById("caption").innerHTML = clicked.caption
+        document.getElementById("description").innerHTML = clicked.description
+        document.getElementById("audio").innerHTML = clicked.audio
+
+        modal.open()
+    });
+
+    // Change the cursor to a pointer when the mouse is over the places layer.
+    map.on('mouseenter', 'points-data', function () {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+
+
+
+    // Change it back to a pointer when it leaves.
+    map.on('mouseleave', 'points-data', function () {
+        map.getCanvas().style.cursor = '';
     });
 });
-
-// Interactivity
-
-    // var coordinates = e.features[0].geometry.coordinates.slice();
-    // var description = e.features[0].properties.description;
-
-    // // Ensure that if the map is zoomed out such that multiple
-    // // copies of the feature are visible, the popup appears
-    // // over the copy being pointed to.
-    // while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
-    // coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
-    // }
-
-    // new mapboxgl.Popup()
-    // .setLngLat(coordinates)
-    // .setHTML(description)
-    // .addTo(map);
 
 
