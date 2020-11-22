92649157b49277d1f27c7b5be965a6ec245a5cd1
gtitov <g.titov@marine-rc.ru>
2020-11-22 12:34:39 +0300

    Custom icons

--- js/main.js
+++ js/main.js
@@ -12,39 +12,66 @@ map.addControl(new mapboxgl.NavigationControl());  // zoom and rotation controls
 
 
 map.on('load', function () {
-    // modals
-    var elem = document.querySelector('#modal1');
-    var modal = M.Modal.init(elem, { endingTop: '100%' });
-
-    // points
+    // Add points
+    // load icons
+    addSVG = function(name, url) {
+        let img = new Image(20,20)
+        img.onload = () => map.addImage(name, img)
+        img.src = url
+    }
+    addSVG('tree-15', 'icons/flag.svg')
+    addSVG('globe', 'icons/globe.svg')
+    addSVG('placeholder', 'icons/placeholder.svg')
+    
+    // load points
     map.addSource('places-points', {
         type: 'vector',
         url: 'mapbox://ghermant.ckhry3x4y0tnv23o4m82j7f3t-7jg6k'
     });
-
+    // draw points
     map.addLayer({
-        'id': 'points-data',
+        'id': 'points-data1',
         'source': 'places-points',
         'source-layer': 'test',
         'type': 'symbol',
         'layout': {
-            'icon-image': 'park-11',
-            'icon-size': 3,
+            'icon-image': ['get', 'icon'],
+            'icon-size': 1,
             'icon-allow-overlap': true,
         },
         'paint': {
             'icon-color': '#FFFF00',
-        }
-        // 'filter': ['==', '$type', 'Point']
+        },
+        'filter': ['==', 'icon', 'tree-15']
+    });
+    map.addLayer({
+        'id': 'points-data2',
+        'source': 'places-points',
+        'source-layer': 'test',
+        'type': 'symbol',
+        'layout': {
+            'icon-image': ['get', 'icon'],
+            'icon-size': 1,
+            'icon-allow-overlap': true,
+        },
+        'paint': {
+            'icon-color': '#FFFF00',
+        },
+        'filter': ['==', 'icon', 'park-15']
     });
 
+    // Activate modals
+    var elem = document.querySelector('#modal1');
+    var modal = M.Modal.init(elem, { endingTop: '100%' });
 
-    map.on('click', 'points-data', function (e) {
+
+    // Set popups
+    map.on('click', 'points-data1', function (e) {
         var clicked = e.features[0].properties
 
         document.getElementById("title").innerHTML = clicked.title
         document.getElementById("address").innerHTML = clicked.address
-        document.getElementById("image").innerHTML = clicked.image
+        document.getElementById("image").src = clicked.image
         document.getElementById("caption").innerHTML = clicked.caption
         document.getElementById("description").innerHTML = clicked.description
         document.getElementById("audio").innerHTML = clicked.audio
@@ -52,15 +79,13 @@ map.on('load', function () {
         modal.open()
     });
 
-    // Change the cursor to a pointer when the mouse is over the places layer.
-    map.on('mouseenter', 'points-data', function () {
+    // change the cursor to a pointer when the mouse is over the places layer
+    map.on('mouseenter', 'points-data1', function () {
         map.getCanvas().style.cursor = 'pointer';
     });
 
-
-
-    // Change it back to a pointer when it leaves.
-    map.on('mouseleave', 'points-data', function () {
+    // change it back to a pointer when it leaves
+    map.on('mouseleave', 'points-data1', function () {
         map.getCanvas().style.cursor = '';
     });
 });
