420ee690c6f00851425aebffc1ea25a52ca42f68
gtitov <g.titov@marine-rc.ru>
2020-11-22 14:00:12 +0300

    Function to add layer

--- js/main.js
+++ js/main.js
@@ -5,89 +5,83 @@ var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11',
     center: [37.6, 55.8],  // starting position
-    zoom: 11  // starting zoom
+    zoom: 11,  // starting zoom
+    attributionControl: false
 });
-map.addControl(new mapboxgl.NavigationControl());  // zoom and rotation controls
-    
+map.addControl(new mapboxgl.NavigationControl({ showCompass: false }));  // zoom controls
+map.addControl(new mapboxgl.AttributionControl({
+    compact: true
+    }));
+
 
 
 map.on('load', function () {
-    // Add points
+    // load points
+    map.addSource('places-points', {
+        type: 'vector',
+        url: 'mapbox://ghermant.ckhry3x4y0tnv23o4m82j7f3t-7jg6k'
+    });
+
     // load icons
-    addSVG = function(name, url) {
-        let img = new Image(20,20)
+    addSVG = function (name, url) {
+        let img = new Image(30, 30)
         img.onload = () => map.addImage(name, img)
         img.src = url
     }
     addSVG('tree-15', 'icons/flag.svg')
     addSVG('globe', 'icons/globe.svg')
     addSVG('placeholder', 'icons/placeholder.svg')
-    
-    // load points
-    map.addSource('places-points', {
-        type: 'vector',
-        url: 'mapbox://ghermant.ckhry3x4y0tnv23o4m82j7f3t-7jg6k'
-    });
-    // draw points
-    map.addLayer({
-        'id': 'points-data1',
-        'source': 'places-points',
-        'source-layer': 'test',
-        'type': 'symbol',
-        'layout': {
-            'icon-image': ['get', 'icon'],
-            'icon-size': 1,
-            'icon-allow-overlap': true,
-        },
-        'paint': {
-            'icon-color': '#FFFF00',
-        },
-        'filter': ['==', 'icon', 'tree-15']
-    });
-    map.addLayer({
-        'id': 'points-data2',
-        'source': 'places-points',
-        'source-layer': 'test',
-        'type': 'symbol',
-        'layout': {
-            'icon-image': ['get', 'icon'],
-            'icon-size': 1,
-            'icon-allow-overlap': true,
-        },
-        'paint': {
-            'icon-color': '#FFFF00',
-        },
-        'filter': ['==', 'icon', 'park-15']
-    });
 
-    // Activate modals
+    // activate modals
     var elem = document.querySelector('#modal1');
-    var modal = M.Modal.init(elem, { endingTop: '100%' });
-
-
-    // Set popups
-    map.on('click', 'points-data1', function (e) {
-        var clicked = e.features[0].properties
-
-        document.getElementById("title").innerHTML = clicked.title
-        document.getElementById("address").innerHTML = clicked.address
-        document.getElementById("image").src = clicked.image
-        document.getElementById("caption").innerHTML = clicked.caption
-        document.getElementById("description").innerHTML = clicked.description
-        document.getElementById("audio").innerHTML = clicked.audio
-
-        modal.open()
+    var modal = M.Modal.init(elem, {
+        // get modal back to start after scroll when closing to open it at start next time 
+        onCloseStart: (element) => element.scrollTop = 0
     });
 
-    // change the cursor to a pointer when the mouse is over the places layer
-    map.on('mouseenter', 'points-data1', function () {
-        map.getCanvas().style.cursor = 'pointer';
-    });
+    // add points layer with popups on map
+    mapPoints = function (layerId, source, sourceLayer, filterValue) {
+        // draw points
+        map.addLayer({
+            'id': layerId,
+            'source': source,
+            'source-layer': sourceLayer,
+            'type': 'symbol',
+            'layout': {
+                'icon-image': ['get', 'icon'],
+                'icon-size': 1,
+                'icon-allow-overlap': true,
+            },
+            'filter': ['==', 'icon', filterValue]
+        });
 
-    // change it back to a pointer when it leaves
-    map.on('mouseleave', 'points-data1', function () {
-        map.getCanvas().style.cursor = '';
-    });
+        // change the cursor to a pointer when the mouse is over the layer
+        map.on('mouseenter', layerId, function () {
+            map.getCanvas().style.cursor = 'pointer';
+        });
+
+        // change it back to a pointer when it leaves
+        map.on('mouseleave', layerId, function () {
+            map.getCanvas().style.cursor = '';
+        });
+
+        // set popups
+        map.on('click', layerId, function (e) {
+            var clicked = e.features[0].properties
+
+            document.getElementById("title").innerHTML = clicked.title
+            document.getElementById("address").innerHTML = clicked.address
+            document.getElementById("image").src = clicked.image
+            document.getElementById("caption").innerHTML = clicked.caption
+            document.getElementById("description").innerHTML = clicked.description
+            document.getElementById("audio").innerHTML = clicked.audio
+            
+            modal.open()
+        });
+    }
+
+    mapPoints('points-data1', 'places-points', 'test', 'tree-15')
+    mapPoints('points-data2', 'places-points', 'test', 'park-15')
 });
 
 
